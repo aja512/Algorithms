2fb7c8a79772970475222b7804dbe0fab4fa7d33
AMANDA JUDY ANDRADE <42480995+aja512@users.noreply.github.com>
2023-05-28 21:05:30 -0700

    Update solutions.py

--- MEDIUM/Maximum Sum of Distinct Subarrays With Length K /solutions.py
+++ MEDIUM/Maximum Sum of Distinct Subarrays With Length K /solutions.py
@@ -1,1 +1,23 @@
-
+class Solution:
+    def maximumSubarraySum(self, nums: List[int], k: int) -> int:
+        MaxSum = 0
+        Sum = 0
+        Cand = set()
+        left = 0
+        for i, num in enumerate(nums):
+            if num in Cand:
+                if nums[left] == num:
+                    left += 1
+                else:
+                    Cand = set([num])
+                    Sum = num
+                    left = i
+                continue
+            Cand.add(num)
+            Sum += num
+            if len(Cand) == k:
+                MaxSum = max(Sum, MaxSum)
+                Sum -= nums[left]
+                Cand.remove(nums[left])
+                left += 1
+        return MaxSum
