0e229f7d7e714bd476a79e43169570cac9cd632e
AMANDA JUDY ANDRADE <42480995+aja512@users.noreply.github.com>
2023-05-28 20:49:54 -0700

    Update solution.py

--- MEDIUM/Valid Palindrome IV /solution.py
+++ MEDIUM/Valid Palindrome IV /solution.py
@@ -1,4 +1,12 @@
 class Solution:
     def makePalindrome(self, s: str) -> bool:
+        res = []
+        if s == s[::-1]:
+            return True
+        for i, j in zip(s,s[::-1]):
+            if i != j and [j,i] not in res:
+                res.append([i,j])
+            if len(res) > 2:
+                return False
+        return True 
         
-        return sum([s[x]!=s[-1-x] for x in range(len(s)//2)]) <= 2
