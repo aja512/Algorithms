9dd6ae72f6d88458b6b7bfefafffd20a698fd86b
AMANDA JUDY ANDRADE <42480995+aja512@users.noreply.github.com>
2023-06-16 14:34:50 -0700

    Update solution.py

--- HARD/Number of Ways to Reorder Array to Get Same BST /solution.py
+++ HARD/Number of Ways to Reorder Array to Get Same BST /solution.py
@@ -1,24 +1,29 @@
 class Solution:
-    # dp(s) = dp(left)*dp(right)*C(n-1,left), left+right=n-1
-    # O(n^2)
-    # top down: split interval; bottom-up: merge interval
     def numOfWays(self, nums: List[int]) -> int:
         mod = 1000000007
         n = len(nums)
-        '''
-        # combination(n,r)
-        def cnr(n,r):
-            if r==0 or n==r: return 1
-            return (cnr(n-1,r)+cnr(n-1,r-1))%mod
-        '''
 
-        leng = [0]*(n+2)
-        cnt = [1]*(n+2)
-        for p in nums[::-1]:
-            h,k = leng[p-1],leng[p+1]
-            t = (cnt[p-1]*cnt[p+1]%mod)*comb(h+k,h)%mod
+
+        # stores the length of each range of numbers, respectively.
+        leng = [0]*(n+2) 
+
+        # stores the count of each range of numbers, respectively.
+        cnt = [1]*(n+2) 
+
+        # Iterate the array in reverse order
+        for p in nums[::-1]: 
+            
+            # For each number p, it calculates the length of the range of numbers to the left of p (stored in h) and the length of the range of numbers to the right of p (stored in k).  
+            h,k = leng[p-1],leng[p+1] 
+
+            # Calculates the number of ways to split the range of numbers from p-h to p+k into two smaller ranges. This is done by multiplying the count of the range of numbers to the left of p (stored in cnt[p-1]), the count of the range of numbers to the right of p (stored in cnt[p+1]), and the combination of h+k and h
+            t = (cnt[p-1]*cnt[p+1]%mod)*comb(h+k,h)%mod 
+
+            # update leng and cnt arrays
             leng[p-h]=leng[p+k]=h+k+1
             cnt[p-h]=cnt[p+k]=t
-        
+
+        # return the number of ways to reorder nums
         return (cnt[1]-1)%mod
+            
             
