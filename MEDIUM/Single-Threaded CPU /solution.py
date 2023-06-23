8cb5b0efcc3d2f67e708ba278faaad1f95ac1c53
AMANDA JUDY ANDRADE <42480995+aja512@users.noreply.github.com>
2023-06-23 11:03:39 -0700

    Update solution.py

--- MEDIUM/Single-Threaded CPU /solution.py
+++ MEDIUM/Single-Threaded CPU /solution.py
@@ -1,32 +1,16 @@
 class Solution:
     def getOrder(self, tasks: List[List[int]]) -> List[int]:
-        # Sort based on min task processing time or min task index.
-        next_task: List[Tuple[int, int]] = []
-        tasks_processing_order: List[int] = []
+        orders = [(enq, pro, i) for i, (enq, pro) in enumerate(tasks)]
+        orders.sort()
+        heap = []
+        idle_on = 0
+        res = []
         
-        # Store task enqueue time, processing time, index.
-        sorted_tasks = [(enqueue, process, idx) for idx, (enqueue, process) in enumerate(tasks)]
-        sorted_tasks.sort()
-        
-        curr_time = 0
-        task_index = 0
-        
-        # Stop when no tasks are left in array and heap.
-        while task_index < len(tasks) or next_task:
-            if not next_task and curr_time < sorted_tasks[task_index][0]:
-                # When the heap is empty, try updating curr_time to next task's enqueue time. 
-                curr_time = sorted_tasks[task_index][0]
-            
-            # Push all the tasks whose enqueueTime <= currtTime into the heap.
-            while task_index < len(sorted_tasks) and curr_time >= sorted_tasks[task_index][0]:
-                _, process_time, original_index = sorted_tasks[task_index]
-                heapq.heappush(next_task, (process_time, original_index))
-                task_index += 1
-            
-            process_time, index = heapq.heappop(next_task)
-            
-            # Complete this task and increment curr_time.
-            curr_time += process_time
-            tasks_processing_order.append(index)
-        
-        return tasks_processing_order
+        for enq, pro, i in orders:
+            while heap and idle_on < enq:
+                pro_prev, i_prev, enq_prev = heapq.heappop(heap)
+                idle_on = max(idle_on, enq_prev) + pro_prev
+                res.append(i_prev)
+
+            heapq.heappush(heap, (pro, i, enq))
+        return res + [i for _, i, _ in sorted(heap)]
