2d128587d8b8bb6fae4f94025090d177cbed1145
AMANDA JUDY ANDRADE <42480995+aja512@users.noreply.github.com>
2023-07-06 13:58:30 -0700

    Update solution.py

--- MEDIUM/Group Anagrams /solution.py
+++ MEDIUM/Group Anagrams /solution.py
@@ -1,10 +1,10 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        answer = collections.defaultdict(list)
+        answer = collections.defaultdict(list) # Mapping character count to list of Anagrams
 
         for s in strs:
-            count = [0] * 26
+            count = [0] * 26 # Counting through the alphabets a-z and how many each alphabet has
             for c in s:
                 count[ord(c) - ord("a")] += 1
-            answer[tuple(count)].append(s)
+            answer[tuple(count)].append(s)  # ASCII Value addition
         return answer.values()
